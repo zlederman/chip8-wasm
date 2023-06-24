75ca6b0816ae21fceafa62f4f6ca2eead4c86ac9
Zachary Lederman <zach-mac@Zacharys-MacBook-Air.local>
2023-06-24 11:53:38 -0500

    moved js into seperate files

---
+++ js/display.js
@@ -0,0 +1,58 @@
+const height = 32;
+const width = 64;
+const PIXEL_SIZE = 16;
+const GRID_COLOR = "#CCCCCC";
+const OFF_COLOR = "#000000";
+const ON_COLOR = "#48ff00";
+const canvas = document.getElementById("chip8-screen");
+
+const ctx = canvas.getContext('2d');
+canvas.height = (PIXEL_SIZE + 1) * height + 1;
+canvas.width = (PIXEL_SIZE + 1) * width + 1;
+
+
+export function drawGrid() {
+    ctx.beginPath();
+    ctx.strokeStyle = GRID_COLOR;
+  
+    // Vertical lines.
+    for (let i = 0; i <= width; i++) {
+      ctx.moveTo(i * (PIXEL_SIZE + 1) + 1, 0);
+      ctx.lineTo(i * (PIXEL_SIZE + 1) + 1, (PIXEL_SIZE + 1) * height + 1);
+    }
+  
+    // Horizontal lines.
+    for (let j = 0; j <= height; j++) {
+      ctx.moveTo(0,                           j * (PIXEL_SIZE + 1) + 1);
+      ctx.lineTo((PIXEL_SIZE + 1) * width + 1, j * (PIXEL_SIZE + 1) + 1);
+    }
+  
+    ctx.stroke();
+  };
+function getIndex(row, column) {
+    return row * width + column;
+};
+  
+export function drawPixels(chip8, memory, mod) {
+    const pixelsPtr = chip8.get_display()
+    const pixels = new Uint8Array(memory.buffer, pixelsPtr, width * height);
+    ctx.beginPath();
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+        const idx = getIndex(row, col);
+
+        ctx.fillStyle = pixels[idx] === mod.PixelState.OFF
+            ? OFF_COLOR
+            : ON_COLOR;
+
+        ctx.fillRect(
+            col * (PIXEL_SIZE + 1) + 1,
+            row * (PIXEL_SIZE + 1) + 1,
+            PIXEL_SIZE,
+            PIXEL_SIZE
+        );
+        }
+    }
+
+    ctx.stroke();
+};
--- js/index.js
+++ js/index.js
@@ -1,72 +1,17 @@
+import { drawGrid, drawPixels } from './display';
 const wasm = import('../pkg')
 const wasm_memory = import('../pkg/index_bg.wasm')
 var memory;
 var chip8;
 var mod;
-height = 32;
-width = 64;
-const PIXEL_SIZE = 16;
-const GRID_COLOR = "#CCCCCC";
-const OFF_COLOR = "#000000";
-const ON_COLOR = "#48ff00";
-const canvas = document.getElementById("chip8-screen");
-
-const ctx = canvas.getContext('2d');
-canvas.height = (PIXEL_SIZE + 1) * height + 1;
-canvas.width = (PIXEL_SIZE + 1) * width + 1;
 
 
-function drawGrid() {
-    ctx.beginPath();
-    ctx.strokeStyle = GRID_COLOR;
-  
-    // Vertical lines.
-    for (let i = 0; i <= width; i++) {
-      ctx.moveTo(i * (PIXEL_SIZE + 1) + 1, 0);
-      ctx.lineTo(i * (PIXEL_SIZE + 1) + 1, (PIXEL_SIZE + 1) * height + 1);
-    }
-  
-    // Horizontal lines.
-    for (let j = 0; j <= height; j++) {
-      ctx.moveTo(0,                           j * (PIXEL_SIZE + 1) + 1);
-      ctx.lineTo((PIXEL_SIZE + 1) * width + 1, j * (PIXEL_SIZE + 1) + 1);
-    }
-  
-    ctx.stroke();
-  };
-function getIndex(row, column) {
-    return row * width + column;
-};
-  
-function drawPixels() {
-    const pixelsPtr = chip8.get_display()
-    const pixels = new Uint8Array(memory.buffer, pixelsPtr, width * height);
-    ctx.beginPath();
-    for (let row = 0; row < height; row++) {
-        for (let col = 0; col < width; col++) {
-        const idx = getIndex(row, col);
-
-        ctx.fillStyle = pixels[idx] === mod.PixelState.OFF
-            ? OFF_COLOR
-            : ON_COLOR;
-
-        ctx.fillRect(
-            col * (PIXEL_SIZE + 1) + 1,
-            row * (PIXEL_SIZE + 1) + 1,
-            PIXEL_SIZE,
-            PIXEL_SIZE
-        );
-        }
-    }
-
-    ctx.stroke();
-};
 function render() {
     for(let i=0; i < 10; i++){
         chip8.tick();
     }
     drawGrid();
-    drawPixels();
+    drawPixels(chip8, memory, mod);
     requestAnimationFrame(render);
 }
 async function initChip8(rom){
@@ -87,7 +32,6 @@ function buf2hex(buffer) { // buffer is an ArrayBuffer
 async function start(){
    mod = await wasm;
    memory = (await wasm_memory).memory
-   console.log(memory)
    let rom = await loadRom();
    chip8 = await initChip8(rom);
    render()
